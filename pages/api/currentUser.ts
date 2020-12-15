026be50ac5c6edad1f98df66ceaf3bb4410b5171
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2020-12-15 18:11:02 -0500

    Switch to mysql from sqlite

--- pages/api/currentUser.ts
+++ pages/api/currentUser.ts
@@ -13,12 +13,15 @@ export default async (
   const token = req.cookies.picker_id;
 
   if (token) {
-    const id = (jwt.verify(token, process.env.APP_SECRET) as {id: string}).id;
+    const id = (jwt.verify(token, process.env.APP_SECRET) as {id: string})?.id;
     const user = await prisma.user.findUnique({where: {id: Number(id)}});
-    res.statusCode = 200;
-    return res.json({
-      currentUser: {id: user.id, email: user.email, username: user.username},
-    });
+
+    if (user) {
+      res.statusCode = 200;
+      return res.json({
+        currentUser: {id: user.id, email: user.email, username: user.username},
+      });
+    }
   }
 
   res.statusCode = 200;
